9341e9ace5fc998cd964ccbc5dccde5013df9dd3
Neal <whitefur@tuta.io>
2024-01-23 16:54:11 -0800

    functions todo

--- taskmanager.js
+++ taskmanager.js
@@ -2,6 +2,23 @@
 TODO:
 - install sortablejs
 
+Functions TODO:
 
+- when button clicked, open new task dialog
+- when task entered and saved, write to file
+- read from file to display task
+
+- allow resorting with sortablejs
+
+- when task marked as done, award xp.money according to array
+- write xp/money to file
+- read from file to display xp/money
+
+- xp bar?
+
+Later TODO:
+- wardrobe function
+- shop function
+- stat counter
 
 */
