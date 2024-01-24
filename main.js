7f5a76f67a4f6a613778aa7fbb359f35f62719af
Neal <whitefur@tuta.io>
2024-01-24 14:40:12 -0800

    create preload & renderer

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    x: 0,
+    y: 0,
+    useContentSize: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile('tasks.html')
-}
+  win.loadFile("tasks.html");
+};
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
